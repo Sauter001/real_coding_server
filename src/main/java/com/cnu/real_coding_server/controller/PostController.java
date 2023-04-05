94b56b54f4cce522777cfb942dfd65a4f7124ac0
Sauter001 <lsj457@naver.com>
2023-04-05 11:33:28 +0900

    conflict 수정 1

--- src/main/java/com/cnu/real_coding_server/controller/PostController.java
+++ src/main/java/com/cnu/real_coding_server/controller/PostController.java
@@ -1,16 +1,8 @@
 package com.cnu.real_coding_server.controller;
 
 import com.cnu.real_coding_server.entity.Post;
-<<<<<<< HEAD
-import com.cnu.real_coding_server.entity.Project;
-import com.cnu.real_coding_server.model.request.PostRequest;
-import com.cnu.real_coding_server.model.request.ProjectRequest;
-import com.cnu.real_coding_server.service.PostService;
-import com.cnu.real_coding_server.service.ProjectService;
-=======
 import com.cnu.real_coding_server.model.request.PostRequest;
 import com.cnu.real_coding_server.service.PostService;
->>>>>>> 4ff27d07932a02d41a1675328b390217c8441fd4
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,23 +10,13 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-<<<<<<< HEAD
 @RequestMapping("/posts") // resources are always in plural.
-@RequiredArgsConstructor
-public class PostController {
-    private final PostService postService;
-    @PostMapping
-    public ResponseEntity<Post> createPost(@RequestBody PostRequest postRequest /* request body required */) {
-        // postRequest body required
-=======
-@RequestMapping("/posts")
 @RequiredArgsConstructor
 public class PostController {
     private final PostService postService;
 
     @PostMapping
     public ResponseEntity<Post> createPost(@RequestBody PostRequest postRequest) {
->>>>>>> 4ff27d07932a02d41a1675328b390217c8441fd4
         return ResponseEntity.ok(postService.createPost(postRequest));
     }
 
@@ -49,11 +31,7 @@ public class PostController {
     }
 
     @PutMapping("/{postId}")
-<<<<<<< HEAD
     public ResponseEntity<Post> updatePost(@PathVariable("postId") Integer postId,
-=======
-    public ResponseEntity<Post> updatePost(@PathVariable("postId")Integer postId,
->>>>>>> 4ff27d07932a02d41a1675328b390217c8441fd4
                                            @RequestBody PostRequest postRequest) {
         return ResponseEntity.ok(postService.updatePost(postId, postRequest).orElse(null));
     }
@@ -61,14 +39,6 @@ public class PostController {
     @DeleteMapping("/{postId}")
     public ResponseEntity<Void> deletePost(@PathVariable("postId") Integer postId) {
         postService.deletePost(postId);
-<<<<<<< HEAD
         return ResponseEntity.noContent().build();
     }
-
-
-=======
-
-        return ResponseEntity.noContent().build();
-    }
->>>>>>> 4ff27d07932a02d41a1675328b390217c8441fd4
 }
